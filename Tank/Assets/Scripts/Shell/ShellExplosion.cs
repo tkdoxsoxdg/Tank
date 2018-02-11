42447206461419c57d84a25e5468ab33113141a1
MYCOMPUTER\IFK_YukiTakada <y.tkdoxsoxdg@gmail.com>
2018-02-11 17:17:30 +0900

    2018/02/11 修正版

--- Tank/Assets/Scripts/Shell/ShellExplosion.cs
+++ Tank/Assets/Scripts/Shell/ShellExplosion.cs
@@ -2,7 +2,6 @@
 
 public class ShellExplosion : MonoBehaviour
 {
-<<<<<<< HEAD
     public LayerMask m_TankMask;                        // 爆発が影響するものをフィルタリングするために使用。ここでは、"プレイヤー" に設定されます。
     public ParticleSystem m_ExplosionParticles;         // 爆発時に再生するパーティクルへの参照
     public AudioSource m_ExplosionAudio;                // 爆発時に再生するオーディオへの参照
@@ -10,15 +9,6 @@ public class ShellExplosion : MonoBehaviour
     public float m_ExplosionForce = 1000f;              // タンクが爆心にある場合に、タンクに与えられる力の量
     public float m_MaxLifeTime = 2f;                    // 砲弾が削除されるまでの秒数
     public float m_ExplosionRadius = 5f;                // タンクに影響を及ぼすことが可能な爆発からの最大距離
-=======
-    public LayerMask m_TankMask;
-    public ParticleSystem m_ExplosionParticles;
-    public AudioSource m_ExplosionAudio;
-    public float m_MaxDamage = 100f;
-    public float m_ExplosionForce = 1000f;
-    public float m_MaxLifeTime = 2f;
-    public float m_ExplosionRadius = 5f;
->>>>>>> 146b9cbf49a0229d80ac3ea3ba59b34c865af1f7
 
 
     private void Start()
@@ -30,52 +20,7 @@ public class ShellExplosion : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-<<<<<<< HEAD
-        // 砲弾の現在の位置から爆破半径内にあるコライダーすべてを集めます
-        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
 
-        // すべてのコライダーを確認します...
-        for (int i = 0; i < colliders.Length; i++)
-        {
-            // ... そして、リジッドボディを見つけます
-            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
-
-            // リジッドボディがなければ、次のコライダーをチェックします
-            if (!targetRigidbody)
-                continue;
-
-            // 爆発の力を加えます
-            targetRigidbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
-
-            // リジッドボディに関連する TankHealth スクリプトを見つけます
-            TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
-
-            // ゲームオブジェクトにアタッチされた TankHealth スクリプトがなければ、次のコライダーをチェックします
-            if (!targetHealth)
-                continue;
-
-            // 砲弾からの距離に基づいて、ターゲットが受けるダメージ量を計算
-            float damage = CalculateDamage(targetRigidbody.position);
-
-            // このダメージをタンクに適用
-            targetHealth.TakeDamage(damage);
-        }
-
-        // 砲弾とパーティクルの親子関係を解除
-        m_ExplosionParticles.transform.parent = null;
-
-        // パーティクルシステムを再生
-        m_ExplosionParticles.Play();
-
-        // 爆発のサウンドエフェクトを再生
-        m_ExplosionAudio.Play();
-
-        // パーティクルが終了したら、パーティクルを伴っていたゲームオブジェクトを破棄します
-        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.main.duration);
-
-        // 砲弾を破棄
-=======
-        // Find all the tanks in an area around the shell and damage them.
 
         Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
 
@@ -106,28 +51,15 @@ public class ShellExplosion : MonoBehaviour
 
         //duration→m_ExplosionParticles.durationはobsolete(廃止)されているため、下記のように「main」を追記すること。2017.3現在。
         Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.main.duration);
->>>>>>> 146b9cbf49a0229d80ac3ea3ba59b34c865af1f7
+
         Destroy(gameObject);
     }
 
 
     private float CalculateDamage(Vector3 targetPosition)
     {
-<<<<<<< HEAD
-        // 砲弾からターゲットまでのベクトルを作成
-        Vector3 explosionToTarget = targetPosition - transform.position;
 
-        // 砲弾からターゲットまでの距離を計算
-        float explosionDistance = explosionToTarget.magnitude;
 
-        //最大距離 (爆破半径) に対するターゲットの距離の比率を計算
-        float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
-
-        // ダメージの最大値と距離の比率に基づいて、ダメージを計算
-        float damage = relativeDistance * m_MaxDamage;
-
-        // ダメージの最小値は常に 0 です。
-=======
         // Calculate the amount of damage a target should take based on it's position.
         Vector3 explosionToTarget = targetPosition - transform.position;
 
@@ -137,7 +69,7 @@ public class ShellExplosion : MonoBehaviour
 
         float damage = relativeDistance * m_MaxDamage;
 
->>>>>>> 146b9cbf49a0229d80ac3ea3ba59b34c865af1f7
+
         damage = Mathf.Max(0f, damage);
 
         return damage;
