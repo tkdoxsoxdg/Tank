a61f377a415954da87af64461a46753a3d88a957
MYCOMPUTER\IFK_YukiTakada <y.tkdoxsoxdg@gmail.com>
2018-04-14 14:27:27 +0900

    Chapter7 complete
    HPが減らず死なないバグ発生

--- Tank/Assets/Scripts/Managers/GameManager.cs
+++ Tank/Assets/Scripts/Managers/GameManager.cs
@@ -5,20 +5,20 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public int m_NumRoundsToWin = 5;        
-    public float m_StartDelay = 3f;         
-    public float m_EndDelay = 3f;           
-    public CameraControl m_CameraControl;   
-    public Text m_MessageText;              
-    public GameObject m_TankPrefab;         
-    public TankManager[] m_Tanks;           
+    public int m_NumRoundsToWin = 5;
+    public float m_StartDelay = 3f;
+    public float m_EndDelay = 3f;
+    public CameraControl m_CameraControl;
+    public Text m_MessageText;
+    public GameObject m_TankPrefab;
+    public TankManager[] m_Tanks;
 
 
-    private int m_RoundNumber;              
-    private WaitForSeconds m_StartWait;     
-    private WaitForSeconds m_EndWait;       
+    private int m_RoundNumber;
+    private WaitForSeconds m_StartWait;
+    private WaitForSeconds m_EndWait;
     private TankManager m_RoundWinner;
-    private TankManager m_GameWinner;       
+    private TankManager m_GameWinner;
 
 
     private void Start()
@@ -66,6 +66,7 @@ public class GameManager : MonoBehaviour
 
         if (m_GameWinner != null)
         {
+            //シーン番号での遷移。オーバーロードメソッド。
             SceneManager.LoadScene(0);
         }
         else
@@ -103,6 +104,20 @@ public class GameManager : MonoBehaviour
 
     private IEnumerator RoundEnding()
     {
+        DisableTankControl();
+
+        m_RoundWinner = null;
+
+        m_RoundWinner = GetRoundWinner();
+
+        if (m_RoundWinner != null)
+            m_RoundWinner.m_Wins++;
+
+        m_GameWinner = GetGameWinner();
+
+        string message = EndMessage();
+        m_MessageText.text = message;
+
         yield return m_EndWait;
     }
 
